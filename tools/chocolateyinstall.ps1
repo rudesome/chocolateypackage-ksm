7c8cf015998825843fb020c0f9abfe1ae0d78c7d
rudesome <rudesome@users.noreply.github.com>
2024-11-27 19:42:03 +0000

    update(version): 1.1.5
    Files changed:
    M	VERSION
    A	keeper-cli.nuspec
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
 $params = @{
-  packageName    = 'glazewm'
-  FileType       = 'msi'
-  SilentArgs     = '/qn /norestart'
-  Url64bit       = 'https://github.com/glzr-io/glazewm/releases/download/v3.4.0/standalone-glazewm-v3.4.0-x64.msi'
-  checksum64     = 'f50faaf4f496edb9c98ff40114ffff8157a02d8b8774d244edb5260eadb3d8e7' 
+  packageName    = 'keeper_cli'
+  FileType       = 'exe'
+  SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /FORCECLOSEAPPLICATION'
+  Url64bit       = 'https://github.com/Keeper-Security/secrets-manager/releases/download/ksm-cli-1.1.5/keeper-secrets-manager-cli-windows-1.1.5.exe'
+  checksum64     = 'b7834910895e56813d335e304ca53b7aaac94591bb799c8bdef676c840164a2c' 
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @params
