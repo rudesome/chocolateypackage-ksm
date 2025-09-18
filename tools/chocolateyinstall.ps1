37ebb220ff0a762911ac913160a5f09ca2dec26c
rudesome <rudesome@users.noreply.github.com>
2025-09-18 09:09:17 +0000

    update(version): 1.1.6
    Files changed:
    M	VERSION
    M	keeper-cli.nuspec
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $params = @{
   packageName    = 'keeper_cli'
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /FORCECLOSEAPPLICATION'
-  Url64bit       = 'https://github.com/Keeper-Security/secrets-manager/releases/download/ksm-cli-1.1.5/keeper-secrets-manager-cli-windows-1.1.5.exe'
-  checksum64     = 'b7834910895e56813d335e304ca53b7aaac94591bb799c8bdef676c840164a2c' 
+  Url64bit       = 'https://github.com/Keeper-Security/secrets-manager/releases/download/ksm-cli-1.1.6/keeper-secrets-manager-cli-windows-1.1.6.exe'
+  checksum64     = '5a4ad981d5bed8628151a6a31b31778e493ea4bb276e5b2a4c662291f7c1a2cc' 
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @params
