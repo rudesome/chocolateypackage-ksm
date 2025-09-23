6cbed153ccd21bb0fa23ccf13f1cb0d64f30fbc2
rudesome <rudesome@users.noreply.github.com>
2025-09-23 09:09:27 +0000

    update(version): 1.1.6
    Files changed:
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $params = @{
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /FORCECLOSEAPPLICATION'
   Url64bit       = 'https://github.com/Keeper-Security/secrets-manager/releases/download/ksm-cli-1.1.6/keeper-secrets-manager-cli-windows-1.1.6.exe'
-  checksum64     = '5a4ad981d5bed8628151a6a31b31778e493ea4bb276e5b2a4c662291f7c1a2cc' 
+  checksum64     = '23012c1d088aedfa68e9e96f481c19f1717d811d8d914c3743c916f7dbe11f5e' 
   checksumType64 = 'sha256'
 }
 Install-ChocolateyPackage @params
